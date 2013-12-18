aa6c996eec5a8c848780cbf3860599f0702cbfee
simonj <jakub.simon@lmc.eu>
2013-12-18 12:44:01 +0100

    compiler - empty function

--- compiler/compiler.js
+++ compiler/compiler.js
@@ -4,10 +4,48 @@ fs = require("fs");
 var compiler = (function(PEG, fs, undefined) {
     var compiler = {};
     compiler.compile = function(program) {
-        var grammar = fs.readFileSync(__dirname + '/javascript.pegjs', "utf-8");
-        var ast = PEG.buildParser(grammar).parse(program);
-        return ast;
+        var ast = compiler.buildAst(program);
+        return compiler.createBytecode(ast);
     };
+
+    compiler.buildAst = function(program) {
+        var grammar = fs.readFileSync(__dirname + '/javascript.pegjs', "utf-8");
+        return PEG.buildParser(grammar).parse(program);
+    };
+
+    compiler.createBytecode = function(ast) {
+//        console.log("creating bytecode");
+//        console.log(ast);
+        var bytecode = [];
+        if (ast.type == "Program") {
+            for(var i = 0; i < ast.elements.length; i++) {
+//                console.log(ast.elements[i]);
+                var elem = ast.elements[i];
+                switch(elem.type) {
+                    case "Function":
+                        compiler.generateFunction(elem, bytecode);
+                        break;
+                    default:
+                        throw new Error ("Type '" + elem.type + "' not allow in top context!")
+                }
+            }
+        } else {
+            throw new Error ("Unknown type '" + ast.type + "', should be Program!")
+        }
+        return bytecode;
+    };
+
+    compiler.generateFunction = function(elem, bytecode) {
+        var fnc = {
+            "name": elem.name,
+            "arguments": elem.params.length,
+            "localVariables": 0, // TODO
+            "instructions": [] // TODO
+        };
+        bytecode.push(fnc);
+    };
+
+
     return compiler;
 })(PEG, fs);
 
--- compiler/compiler_test.js
+++ compiler/compiler_test.js
@@ -3,7 +3,15 @@ var compiler = require('./compiler.js');
 
 describe('compiler', function() {
     it('should parse empty program', function(){
-        var out = compiler.compile("");
+        var out = compiler.buildAst("");
         assert.deepEqual(out, {"type": "Program", "elements": []})
+    });
+
+    it('should compile empty function ', function(){
+        var out = compiler.compile("function main(){}");
+        assert.deepEqual(out, [{"name": "main",
+            "arguments": 0,
+            "localVariables": 0,
+            "instructions": []}])
     });
 });
